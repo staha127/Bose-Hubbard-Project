a415955269e433f7954d56c2604ad767d59468f6
Sara Taha <staha@tcd.ie>
2026-08-27 14:42:38 +0100

    Delete __init__.py

--- __init__.py
+++
@@ -1,9 +0,0 @@
-
-from .hamiltonian_1d import ReducedBoseHubbard
-from .dynamics_1d import BoseHubbardDynamics
-from .hamiltonian_2d import ReducedBoseHubbard2D
-from .dynamics_2d import BoseHubbardDynamics2D
-
-__all__ = ["ReducedBoseHubbard", "BoseHubbardDynamics", "ReducedBoseHubbard2D", "BoseHubbardDynamics2D"]
-
-__version__ = "0.1.0"
